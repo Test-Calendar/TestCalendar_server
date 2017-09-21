b7a4453a279f200362125aaf937be692099c164b
KO-YAMAKO <ko.foot1103@gmail.com>
2017-09-21 23:53:02 +0900

    viewsの編集

--- server/api/views.py
+++ server/api/views.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONPerser
+from api.models import Study, Task, Test, TimeZone
+from api.serializer import StudySerializer
 
-# Create your views here.
+def study_list(request):
+
+    if request.method == 'GET':
+        studys = Study.objects.all()
+        serializer = StudySerializer(studys, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+
