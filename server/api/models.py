0af968cc4437a4572a092ed9b8489409f38f167d
KO-YAMAKO <ko.foot1103@gmail.com>
2017-09-21 23:04:25 +0900

    server/settingの編集 api, restframeworkをinstallappsに追加, modelsの編集

--- server/api/models.py
+++ server/api/models.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = CharField()
+    start = DateTimeField()
+    end = DateTimeField()
+
+class Test(models.Model):
+    name = CharField()
+    stype = IntegerField()
+    studyTime = IntegerField()
+    start = DateTimeField()
+
+class Timezone(models.Model):
+    start = DateTimeField()
+    end = DateTimeField()
+
+
+
+class Study(models.Model):
+    name = CharField()
+    stype = IntegerField()
+    start = DateTimeField()
+    end = DateTimeField()
+
+
