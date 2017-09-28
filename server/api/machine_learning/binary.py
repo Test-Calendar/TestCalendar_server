61ab5fe6ff482ae406096304f0683b9ecd79070b
TakahiroSono <taka.sono889@gmail.com>
2017-09-28 18:55:49 +0900

    2次元配列に対応

--- server/api/machine_learning/binary.py
+++ server/api/machine_learning/binary.py
@@ -2,15 +2,25 @@ import cvxpy
 import numpy as np
 
 def binary(size, weight, capacity):
-	x = cvxpy.Int(size.shape[0])
-	objective = cvxpy.Maximize(weight * x)
-	constraints = [capacity >= size * x]
-	constraints += [x >= 0, x <= 2]
-	prob = cvxpy.Problem(objective, constraints)
-	prob.solve(solver=cvxpy.ECOS_BB)
-	result = [round(ix[0, 0]) for ix in x.value]
+	for i in size:
+		x = cvxpy.Int(i.shape[0])
+		objective = cvxpy.Maximize(weight * x)
+		constraints = [capacity >= size * x]
+		constraints += [x >= 0, x <= 1]
+		prob = cvxpy.Problem(objective, constraints)
+		prob.solve(solver=cvxpy.ECOS_BB)
+		result = [round(ix[0, 0]) for ix in x.value]
+		print("result", result)
 
-	return result
+	# x = cvxpy.Int(size.shape[0])
+	# objective = cvxpy.Maximize(weight * x)
+	# constraints = [capacity >= size * x]
+	# constraints += [x >= 0, x <= 2]
+	# prob = cvxpy.Problem(objective, constraints)
+	# prob.solve(solver=cvxpy.ECOS_BB)
+	# result = [round(ix[0, 0]) for ix in x.value]
 
-# if __name__ == "__main__":
-# 	binary()
+	# return result
+
+if __name__ == "__main__":
+	binary()
