906d913b17c71e7fb5e2748c0a1aa9db0f89e5bb
TakahiroSono <taka.sono889@gmail.com>
2017-09-29 01:49:38 +0900

    試験用データベース作成

--- server/api/admin.py
+++ server/api/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import Study, TimeZone, Task, Test
 
 # Register your models here.
+admin.site.register(Study)
+admin.site.register(TimeZone)
+admin.site.register(Task)
+admin.site.register(Test)
