8c3cac6bee95ecf341b8e95fe4c3e7655cf354d6
TakahiroSono <taka.sono889@gmail.com>
2017-09-28 19:30:17 +0900

    コメントアウトの削除 戻り値を設定

--- server/api/machine_learning/binary.py
+++ server/api/machine_learning/binary.py
@@ -2,25 +2,19 @@ import cvxpy
 import numpy as np
 
 def binary(size, weight, capacity):
+	result = []
 	for i in size:
 		x = cvxpy.Int(i.shape[0])
 		objective = cvxpy.Maximize(weight * x)
+		#
 		constraints = [capacity >= size * x]
 		constraints += [x >= 0, x <= 1]
+		#
 		prob = cvxpy.Problem(objective, constraints)
 		prob.solve(solver=cvxpy.ECOS_BB)
-		result = [round(ix[0, 0]) for ix in x.value]
-		print("result", result)
+		result.append([round(ix[0, 0]) for ix in x.value])
 
-	# x = cvxpy.Int(size.shape[0])
-	# objective = cvxpy.Maximize(weight * x)
-	# constraints = [capacity >= size * x]
-	# constraints += [x >= 0, x <= 2]
-	# prob = cvxpy.Problem(objective, constraints)
-	# prob.solve(solver=cvxpy.ECOS_BB)
-	# result = [round(ix[0, 0]) for ix in x.value]
-
-	# return result
+	return result
 
 if __name__ == "__main__":
 	binary()
