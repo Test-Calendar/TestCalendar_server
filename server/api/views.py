551ebef524fad4b4525362dd5c2befeb7e120de9
TakahiroSono <taka.sono889@gmail.com>
2017-10-18 20:04:15 +0900

    APIの処理完成

--- server/api/views.py
+++ server/api/views.py
@@ -5,10 +5,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from api.models import Study, Task, Test, TimeZone
-from api.serializers import StudySerializer, TaskSerializer, TestSerializer
+from api.machine_learning.models import Task, Test, TimeZone
+from api.models import Study
+from api.serializers import StudySerializer, TaskSerializer, TestSerializer, TimeZoneSerializer
 
-# from machine_learning.processing import processing
+from machine_learning.processing import processing
 
 @csrf_exempt
 def study_list(request):
@@ -18,14 +19,6 @@ def study_list(request):
         serializer = StudySerializer(studys, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-# @csrf_exempt
-# def test_list(request):
-#
-#     if request.method == 'GET':
-#         tests = Test.objects.all()
-#         serializer = StudySerializer(tests, many=True)
-#         return JsonResponse(serializer.data, safe=False)
-
 @csrf_exempt
 def study_add(request):
 
@@ -33,16 +26,38 @@ def study_add(request):
         data = JSONPerser().parse(request.data)
         tasks = data.tasks
         tests = data.tests
+        """"""
+        task_serializer = []
+        test_serializer = []
+        """"""
+
+        time_zone = TimeZoneSerializer(data=data.time_zone)
+
         for task in tasks:
-            task_serializer = TaskSerializer(data=task)
-            task_serializer.save()
+            # task_serializer = TaskSerializer(data=task)
+            task_serializer.append(TaskSerializer(data=task))
+            # task_serializer.save()
 
         for test in tests:
-            test_serializer = TestSerialzier(data=tests)
-            test_serializer.save()
+            # test_serializer = TestSerialzier(data=tests)
+            test_serializer.append(TestSerialzier(data=tests))
+            # test_serializer.save()
 
         """
         insert your function here
         """
+        schedule_list = processing(task_list=task_serializer, test_list=test_serializer, time_zones=time_zone)
+
+        for schedule_days, i in zip(schedule_list, range(len(schedule_list))):
+            times = pd.period_range(
+                start=today + datetime.timedelta(days=i + 1),
+                end=today + datetime.timedelta(days=i + 1) + datetime.timedelta(hours=time_zone.get_hour()),
+                freq = 'H')
+            schedule = [i for i in schedule_days if i != 'none']
+            for sch, time in zip(schedule, times.values[:,0]):
+                schedule_upload = Study(name=sche.name, stype=sche.stype, start=time, end=time + datetime.timedelta(hours=1))
+                schedule_upload.save()
+
+        """"""
 
         return Response(status=status.HTTP_200_OK)
