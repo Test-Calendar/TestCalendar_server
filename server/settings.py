5db83124630317670dd5aa9cefaa747eeeaa3cdc
TakahiroSono <taka.sono889@gmail.com>
2017-10-31 23:31:59 +0900

    database sqlite

--- server/settings.py
+++ server/settings.py
@@ -79,28 +79,27 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+# if "COMPUTER-NAME" in hostname:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
 #     }
-# }
-if "COMPUTER-NAME" in hostname:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-    ALLOWED_HOSTS = []
-else:
-    import dj_database_url
-    db_from_env = dj_database_url.config()
-    DATABASES = {
-        'default': dj_database_url.config()
-    }
-
-ALLOWED_HOSTS = ['*']
+#     ALLOWED_HOSTS = []
+# else:
+#     import dj_database_url
+#     db_from_env = dj_database_url.config()
+#     DATABASES = {
+#         'default': dj_database_url.config()
+#     }
+#     ALLOWED_HOSTS = ['*']
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
