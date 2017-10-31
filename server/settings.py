e12fd5c0e0bc88fd11311e06db901a053c61c9f1
TakahiroSono <taka.sono889@gmail.com>
2017-10-31 23:18:13 +0900

    インデントを修正

--- server/settings.py
+++ server/settings.py
@@ -99,7 +99,8 @@ else:
     DATABASES = {
         'default': dj_database_url.config()
     }
-    ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['*']
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
