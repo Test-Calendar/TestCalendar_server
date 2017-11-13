89b1ee64ba3e915cc4659360daa080d9b05a67b1
TakahiroSono <taka.sono889@gmail.com>
2017-11-13 19:19:12 +0900

    INSTALLED_APPSとurlを変更

--- server/urls.py
+++ server/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
-    # url(r'^api/', include('api.urls')),
 ]
