e0ddd950a4528368a4a972da86e6dd3290769cf4
TakahiroSono <taka.sono889@gmail.com>
2017-11-14 17:34:08 +0900

    微修正

--- api/machine_learning/test.py
+++ api/machine_learning/test.py
@@ -6,21 +6,21 @@ from models import Task, Test, TimeZone, Study
 
 def tests_process():
     tasks = []
-    for i in range(5):
-        day_start = datetime.datetime(2017, 11, 6 + i, 17)
-        day_end = datetime.datetime(2017, 11, 6 + i, 20)
+    for i in range(3):
+        day_start = datetime.datetime(2017, 11, 13 + i, 17)
+        day_end = datetime.datetime(2017, 11, 13 + i, 20)
         tasks.append(Task("task", day_start, day_end))
     tests = []
-    tests.append(Test("math", 2, 8, datetime.datetime(2017, 11, 11)))
-    tests.append(Test("math", 1, 6, datetime.datetime(2017, 11, 10)))
-    tests.append(Test("math", 1, 8, datetime.datetime(2017, 11, 14)))
-    tests.append(Test("poko", 1, 7, datetime.datetime(2017, 11, 10)))
-    tests.append(Test("poko", 2, 7, datetime.datetime(2017, 11, 10)))
-    tests.append(Test("taku", 2, 7, datetime.datetime(2017, 11, 12)))
-    tests.append(Test("taku", 1, 7, datetime.datetime(2017, 11, 12)))
-    tests.append(Test("poko", 2, 9, datetime.datetime(2017, 11, 13)))
+    tests.append(Test("math", 1, 6, datetime.datetime(2017, 11, 20)))
+    tests.append(Test("poko", 1, 7, datetime.datetime(2017, 11, 20)))
+    tests.append(Test("poko", 2, 7, datetime.datetime(2017, 11, 20)))
+    tests.append(Test("math", 2, 8, datetime.datetime(2017, 11, 21)))
+    tests.append(Test("taku", 2, 7, datetime.datetime(2017, 11, 22)))
+    tests.append(Test("taku", 1, 7, datetime.datetime(2017, 11, 22)))
+    tests.append(Test("poko", 1, 9, datetime.datetime(2017, 11, 23)))
+    tests.append(Test("math", 1, 8, datetime.datetime(2017, 11, 24)))
 
-    time_zone = TimeZone(18, 2)
+    time_zone = TimeZone(19, 2)
     processing.processing(task_list=tasks, test_list=tests, time_zones=time_zone)
 
 if __name__ == '__main__':
