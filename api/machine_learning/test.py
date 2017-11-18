9a42d5ee0aeb038b4d300d5466f6699b3ab0bd59
TakahiroSono <taka.sono889@gmail.com>
2017-11-19 01:41:41 +0900

    test用の日付設定を一般化

--- api/machine_learning/test.py
+++ api/machine_learning/test.py
@@ -4,21 +4,23 @@ import datetime
 import processing
 from models import Task, Test, TimeZone, Study
 
+today = datetime.datetime.today()
+
 def tests_process():
     tasks = []
     for i in range(3):
-        day_start = datetime.datetime(2017, 11, 13 + i, 17)
-        day_end = datetime.datetime(2017, 11, 13 + i, 20)
+        day_start = today.date() + datetime.timedelta(days=2 + i, hours=17)
+        day_end = today.date() + datetime.timedelta(days=2 + i, hours=20)
         tasks.append(Task("task", day_start, day_end))
     tests = []
-    tests.append(Test("math", 1, 6, datetime.datetime(2017, 11, 20)))
-    tests.append(Test("poko", 1, 7, datetime.datetime(2017, 11, 20)))
-    tests.append(Test("poko", 2, 7, datetime.datetime(2017, 11, 20)))
-    tests.append(Test("math", 2, 8, datetime.datetime(2017, 11, 21)))
-    tests.append(Test("taku", 2, 7, datetime.datetime(2017, 11, 22)))
-    tests.append(Test("taku", 1, 7, datetime.datetime(2017, 11, 22)))
-    tests.append(Test("poko", 1, 9, datetime.datetime(2017, 11, 23)))
-    tests.append(Test("math", 1, 8, datetime.datetime(2017, 11, 24)))
+    tests.append(Test("math", 1, 6, today + datetime.timedelta(days=7)))
+    tests.append(Test("poko", 1, 7, today + datetime.timedelta(days=7)))
+    tests.append(Test("poko", 2, 7, today + datetime.timedelta(days=7)))
+    tests.append(Test("math", 2, 8, today + datetime.timedelta(days=8)))
+    tests.append(Test("taku", 2, 7, today + datetime.timedelta(days=8)))
+    tests.append(Test("taku", 1, 7, today + datetime.timedelta(days=9)))
+    tests.append(Test("poko", 1, 9, today + datetime.timedelta(days=9)))
+    tests.append(Test("math", 1, 8, today + datetime.timedelta(days=10)))
 
     time_zone = TimeZone(19, 2)
     processing.processing(task_list=tasks, test_list=tests, time_zones=time_zone)
