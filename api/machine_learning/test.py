4e3434c6ee284a9884032cad3815f6c57e59c2e9
TakahiroSono <taka.sono889@gmail.com>
2017-11-19 01:46:22 +0900

    再度修正

--- api/machine_learning/test.py
+++ api/machine_learning/test.py
@@ -4,23 +4,23 @@ import datetime
 import processing
 from models import Task, Test, TimeZone, Study
 
-today = datetime.datetime.today()
+today = datetime.datetime.today().date()
 
 def tests_process():
     tasks = []
     for i in range(3):
-        day_start = today.date() + datetime.timedelta(days=2 + i, hours=17)
-        day_end = today.date() + datetime.timedelta(days=2 + i, hours=20)
+        day_start = today + datetime.timedelta(days=2 + i, hours=17)
+        day_end = today + datetime.timedelta(days=2 + i, hours=20)
         tasks.append(Task("task", day_start, day_end))
     tests = []
-    tests.append(Test("math", 1, 6, today + datetime.timedelta(days=7)))
-    tests.append(Test("poko", 1, 7, today + datetime.timedelta(days=7)))
-    tests.append(Test("poko", 2, 7, today + datetime.timedelta(days=7)))
-    tests.append(Test("math", 2, 8, today + datetime.timedelta(days=8)))
-    tests.append(Test("taku", 2, 7, today + datetime.timedelta(days=8)))
-    tests.append(Test("taku", 1, 7, today + datetime.timedelta(days=9)))
-    tests.append(Test("poko", 1, 9, today + datetime.timedelta(days=9)))
-    tests.append(Test("math", 1, 8, today + datetime.timedelta(days=10)))
+    tests.append(Test("math", 1, 6, datetime.datetime.combine(today.date(), datetime.time()) + datetime.timedelta(days=7)))
+    tests.append(Test("poko", 1, 7, datetime.datetime.combine(today.date(), datetime.time()) + datetime.timedelta(days=7)))
+    tests.append(Test("poko", 2, 7, datetime.datetime.combine(today.date(), datetime.time()) + datetime.timedelta(days=7)))
+    tests.append(Test("math", 2, 8, datetime.datetime.combine(today.date(), datetime.time()) + datetime.timedelta(days=8)))
+    tests.append(Test("taku", 2, 7, datetime.datetime.combine(today.date(), datetime.time()) + datetime.timedelta(days=8)))
+    tests.append(Test("taku", 1, 7, datetime.datetime.combine(today.date(), datetime.time()) + datetime.timedelta(days=9)))
+    tests.append(Test("poko", 1, 9, datetime.datetime.combine(today.date(), datetime.time()) + datetime.timedelta(days=9)))
+    tests.append(Test("math", 1, 8, datetime.datetime.combine(today.date(), datetime.time()) + datetime.timedelta(days=10)))
 
     time_zone = TimeZone(19, 2)
     processing.processing(task_list=tasks, test_list=tests, time_zones=time_zone)
